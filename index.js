60954f07221e7daef7d7f01f29c73df91aee228f
Amman Chopadiya <ammanchopadiya15@gmail.com>
2023-12-17 12:54:21 +0530

    Adding lap functionality

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var time = document.getElementById("time");
 var green = document.getElementById("green");
 var red = document.getElementById("red");
 var yellow = document.getElementById("yellow");
+var inner = document.getElementById("inner");
 var btn_status = 0;
 var hour = 0;
 var min = 0;
 var sec = 0;
 green.addEventListener("click", ()=>{
     if (btn_status==0){
+        yellow.disabled=false;
         outBox.style.backgroundColor = "#00A800";
         green.setAttribute('class', 'btn red');
         btn_status = 1;
@@ -38,6 +40,15 @@ green.addEventListener("click", ()=>{
     }
 });
 
+yellow.addEventListener("click",()=>{
+    var lap_time = hour+":"+min+":"+sec;
+    inner.innerHTML+= "<br>"+lap_time;
+    outBox.style.backgroundColor = "#FFD700";
+    setTimeout(()=>{
+        outBox.style.backgroundColor = "#00A800";
+    },500);
+})
+
 red.addEventListener("click",()=>{
     outBox.style.backgroundColor = "#111111";
     sec = min = hour = 0;
@@ -46,4 +57,6 @@ red.addEventListener("click",()=>{
     green.innerHTML = "START";
     clearInterval(timer);
     time.innerHTML = hour+":"+min+":"+sec;
+    inner.innerHTML = "LAPS"
+    yellow.disabled=true;
 })
